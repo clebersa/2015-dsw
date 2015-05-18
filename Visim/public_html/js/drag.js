63682148306f929d804b224f90b75e67d30b95c2
clebersa <cleber.93cd@gmail.com>
2015-05-18 01:10:22 -0300

    - Alterando/corrigindo alguns dos requisitos implementados antes e adicionando mais algumas modificações.

--- Visim/public_html/js/drag.js
+++ Visim/public_html/js/drag.js
@@ -1,4 +1,23 @@
 $(function () {
     $('#maior').resizable();
-    $("#image").draggable();
+
+    $("#image").mousedown(function () {
+        $(window).mousemove(function () {
+            var alturaDiv = $("#maior").innerHeight();
+            var larguraDiv = $("#maior").innerWidth();
+            var alturaImagem = $("#image").innerHeight();
+            var larguraImagem = $("#image").innerWidth();
+            if (alturaImagem > alturaDiv || larguraImagem > larguraDiv) {
+                if (alturaImagem > alturaDiv) {
+                    if (larguraImagem > larguraDiv) {
+                        $("#image").draggable();
+                    } else {
+                        $("#image").draggable({axis: "y"});
+                    }
+                } else {
+                    $("#image").draggable({axis: "x"});
+                }
+            }
+        });
+    });
 });
--- Visim/public_html/js/load_image.js
+++ Visim/public_html/js/load_image.js
@@ -8,48 +8,53 @@ $(function () {
         }
     });
 
-    $("#image_url_file").change(function (event) {
-        var value = $("#image_url_file").val();
-        if(value !== ""){
-            $("#image_url_text").val(value);
-        }
-    });
-
     $("#carregarImagem").click(function (event) {
-        var image_url = $("#image_url_text").val();
+
         var allowedTypes = ["image/jpeg", "image/png"];
         var allowed = false;
 
-        if (image_url === "") {
-            alert("Por favor, especifique uma imagem para visualização!");
-            return;
-        }
-
         var reader = new FileReader();
         reader.onload = getLoadedImage;
-        try {
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function () {
-                if (this.readyState === 4 && this.status === 200) {
-                    var file = this.response;
-                    for (var index = 0; index < allowedTypes.length; index++) {
-                        if (allowedTypes[index] === file.type) {
-                            allowed = true;
-                            break;
+
+        if ($("#image_url_file").val() == "") {
+            var image_url = $("#image_url_text").val();
+            if (image_url === "") {
+                alert("Por favor, especifique uma imagem para visualização!");
+                return;
+            }
+            try {
+                var xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function () {
+                    if (this.readyState === 4) {
+                        if (this.status === 200) {
+                            var file = this.response;
+                            for (var index = 0; index < allowedTypes.length; index++) {
+                                if (allowedTypes[index] === file.type) {
+                                    allowed = true;
+                                    break;
+                                }
+                            }
+                            if (!allowed) {
+                                alert("Imagem inválida! Por favor, informe uma imagem JPEG ou PNG.");
+                                return;
+                            }
+                            reader.readAsDataURL(this.response);
+                            reader.onloadend = function () {
+                                $("image").attr("src", this.result);
+                                alignImage();
+                            }
+                        } else {
+                            alert("Não foi possível carregar a imagem a partir da URL informada!");
                         }
                     }
-                    if (!allowed) {
-                        alert("Imagem inválida! Por favor, informe uma imagem JPEG ou PNG.");
-                        return;
-                    }
-                    reader.readAsDataURL(this.response);
                 }
-            }
-            xhr.open('GET', image_url);
-            xhr.responseType = 'blob';
+                xhr.open('GET', image_url);
+                xhr.responseType = 'blob';
 
-            xhr.send(null);
-        } catch (error) {
+                xhr.send(null);
+            } catch (error) {
+            }
+        } else {
             var file = $("#image_url_file").prop("files")[0];
 
             for (var index = 0; index < allowedTypes.length; index++) {
@@ -63,9 +68,11 @@ $(function () {
                 return;
             }
             try {
-                console.log($("#image").innerWidth() + " x "
-                        + $("#image").innerHeight());
                 reader.readAsDataURL(file);
+                reader.onloadend = function () {
+                    $("image").attr("src", this.result);
+                    alignImage();
+                }
             } catch (error2) {
                 alert("Não foi possível carregar a imagem.");
             }
@@ -74,6 +81,19 @@ $(function () {
 });
 
 function getLoadedImage(event) {
-    $("#image").attr("src", event.target.result)
+    $("#image").attr("src", event.target.result);
 }
 
+function alignImage(event) {
+
+    var ajusteHorizontal = ($("#image").height()) / 2;
+    var ajusteVertical = ($("#image").width()) / 2;
+
+    ajusteHorizontal = '-' + ajusteHorizontal + 'px';
+    ajusteVertical = '-' + ajusteVertical + 'px';
+
+    $("#image").addClass("js-fix").css({
+        "margin-left": ajusteHorizontal,
+        "margin-top": ajusteVertical
+    });
+}
