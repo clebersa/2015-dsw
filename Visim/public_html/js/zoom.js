6abbbcadd3546e704f187c6e8f2b549c64e15738
clebersa <cleber.93cd@gmail.com>
2015-05-24 00:37:05 -0300

    - Corrigindo zoom in.

--- Visim/public_html/js/zoom.js
+++ Visim/public_html/js/zoom.js
@@ -1,17 +1,81 @@
 $(function () {
     $("#ZoomIn").click(function (event) {
-        var new_width = $("#image").width() * 1.2;
-        var new_height = $("#image").height() * 1.2;
+        var current_left = parseInt($("#image").css("left"));
+        var current_top = parseInt($("#image").css("top"));
+
+        var current_width = $("#image").width();
+        var current_height = $("#image").height();
+
+        var new_width = current_width * 1.2;
+        var new_height = current_height * 1.2;
+
+        var width_distance = $("#maior").width() / 2 - current_left;
+        var height_distance = $("#maior").height() / 2 - current_top;
+
+        var new_left;
+        if (width_distance > current_width) {
+            console.log("width_distance > current_width");
+            new_left = current_left - (new_width - current_width) 
+                    - (width_distance - current_width) * 0.2 / 2;
+        } else if (width_distance === current_width) {
+            console.log("width_distance === current_width");
+            new_left = current_left - (current_width - new_width);
+        } else {
+            if (width_distance > 0) {
+                console.log("width_distance > 0");
+                new_left = current_left - width_distance * 0.2;
+            } else if (width_distance === 0) {
+                console.log("width_distance === 0");
+                new_left = current_left;
+            } else {
+                console.log("width_distance < 0");
+                new_left = current_left - (width_distance * 0.2) / 2
+            }
+        }
+        
+        var new_top;
+        if (height_distance > current_height) {
+            console.log("height_distance > current_height");
+            new_top = current_top - (new_height - current_height)
+                    - (height_distance - current_height) * 0.2/2;
+        } else if (height_distance === current_height) {
+            console.log("height_distance === current_height");
+            new_top = current_top - (current_height - new_height);
+        } else {
+            if (height_distance > 0) {
+                console.log("height_distance > 0");
+                new_top = current_top - height_distance * 0.2;
+            } else if (height_distance === 0) {
+                console.log("height_distance === 0");
+                new_top = current_top;
+            } else {
+                console.log("height_distance < 0");
+                new_top = current_top - (height_distance * 0.2) / 2
+            }
+        }
+
         $("#image").width(new_width);
         $("#image").height(new_height);
-        alignImage();
+        $("#image").css({
+            "top": new_top,
+            "left": new_left
+        });
     });
 
     $("#ZoomOut").click(function (event) {
+        var current_width = $("#image").width();
+        var current_height = $("#image").height();
         var new_width = $("#image").width() * 0.9;
         var new_height = $("#image").height() * 0.9;
         $("#image").width(new_width);
         $("#image").height(new_height);
-        alignImage();
+        var new_top = parseInt($("#image").css("top")) - (new_height - current_height) / 2;
+        var new_left = parseInt($("#image").css("left")) - (new_width - current_width) / 2;
+        console.log($("#image").css("top") + " -> " + new_top);
+        console.log($("#image").css("left") + " -> " + new_left);
+        $("#image").css({
+            "top": new_top,
+            "left": new_left
+        });
     });
 });
